8839a4515f7ed072e118d3902a4d9924fa13e99d
jsd <whtjdejr0824@naver.com>
2020-08-24 18:22:49 +0900

    make update/delete

--- board/urls.py
+++ board/urls.py
@@ -9,7 +9,8 @@ urlpatterns = [
     path('<int:pk>', BoardViewDV.as_view(), name='details'),
     # Example: /board/add
     path('add/', BoardCreateView.as_view(), name="add"),
-
-
-
+    # Example: /bookmark/99/update/
+     path('<int:pk>/update/', BookmarkUpdateView.as_view(), name="update"),
+     # Example: /bookmark/99/delete/
+     path('<int:pk>/delete/', BookmarkDeleteView.as_view(), name="delete"),
 ]
--- board/views.py
+++ board/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from django.views.generic import TemplateView, ListView, DetailView
 from board.models import BoardLI
 from django.views.generic import ListView, DetailView
-from django.views.generic.dates import ArchiveIndexView, YearArchiveView, MonthArchiveView, DayArchiveView, TodayArchiveView
+from django.views.generic.dates import ArchiveIndexView, YearArchiveView, MonthArchiveView, DayArchiveView, \
+    TodayArchiveView
 from django.views.generic import CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
+
 
 # TemplateView
 class BoardView(ListView):
@@ -14,10 +16,12 @@ class BoardView(ListView):
     context_object_name = "boards"
     paginate_by = 10
 
+
 class BoardViewDV(DetailView):
     template_name = 'board_detail.html'
     model = BoardLI
     context_object_name = 'details'
+
 
 class BoardCreateView(CreateView):
     model = BoardLI
@@ -29,3 +33,13 @@ class BoardCreateView(CreateView):
     def form_valid(self, form):
         return super().form_valid(form)
 
+
+class BookmarkUpdateView(UpdateView):
+    model = BoardLI
+    fields = ['name', 'title', 'content']  # 폼 모델에 사용할 필드  폼 모델 자동 생성
+    success_url = reverse_lazy('board:board')
+
+
+class BookmarkDeleteView(DeleteView):
+    fields = ['name', 'title', 'content']  # 폼 모델에 사용할 필드  폼 모델 자동 생성
+    success_url = reverse_lazy('board:board')
