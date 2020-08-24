7126bb71dfa0c29d7be15f983341f51255b29da7
jsd <whtjdejr0824@naver.com>
2020-08-24 10:27:33 +0900

    borad making 1

--- board/models.py
+++ board/models.py
@@ -9,3 +9,14 @@ class BoardLI(models.Model):
 
     def __str__(self):
         return self.name
+
+    def __str__(self): return self.title
+
+    def get_absoulte_url(self):  # 현재 데이터의 절대 경로 추출
+        return reverse('blog:post_detail', args=(self.slug,))
+
+    def get_previous(self): # 이전 데이터 추출
+        return self.get_previous_by_modify_dt()
+
+    def get_next(self): # 다음 데이터 추출
+        return self.get_next_by_modify_dt()
--- board/urls.py
+++ board/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, re_path
-from board.views import BoardView, BoardViewDK
+from board.views import *
 from django.contrib import admin
 
 app_name = 'board'
@@ -8,4 +8,16 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', BoardView.as_view(), name='board'),
     path('<int:pk>/', BoardViewDK.as_view(), name='details'),
+
+    # Example: /board/archive/
+    path('archive/', BoardAV.as_view(), name='Board_archive'),
+
+    # Example: /board/archive/2020/
+    path('archive/<int:year>/', BoardYAV.as_view(), name='post_year_archive'),
+    # Example: /board/archive/2020/Aug/
+    path('archive/<int:year>/<str:month>/', BoardMAV.as_view(), name='post_month_archive'),
+    # Example: /board/archive/2019/Aug/25/
+    path('archive/<int:year>/<str:month>/<int:day>/', BoardDAV.as_view(), name='post_day_archive'),
+    # Example: /board/archive/today/
+    path('archive/today/', BoardTAV.as_view(), name='post_today_archive'),
 ]
